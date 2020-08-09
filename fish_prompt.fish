282c547907a92fcf20c585c2843bd87d9086d858
toshi75 <jaf04435@gmail.com>
2020-08-10 03:36:50 +0900

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -8,11 +8,11 @@ function fish_prompt --description 'Prompt:noisyfish'
 
   # Line 2
     set -l USE JP  # JP or not.
-    set_color bryellow ;echo -n 'fish'
-    set_color brred    ;echo -n "["
-    set_color brcyan   ;echo -n (date '+%m')
-    set_color bryellow ;echo -n '/'
-    set_color brcyan   ;echo -n (date +%d)
+    set_color ffff00 ;echo -n 'fish'
+    set_color ff9900 ;echo -n "["
+    set_color 00ffff ;echo -n (date '+%m')
+    set_color ffff00 ;echo -n '/'
+    set_color 00ffff ;echo -n (date +%d)
     if [ "$USE" = "JP" ]
                         echo -n (date '+%u' |
                                  sed -e 's|1|㈪|g' \
@@ -35,26 +35,26 @@ function fish_prompt --description 'Prompt:noisyfish'
     end
                                      echo -n ' '
                         echo -n (date +%H)
-    set_color bryellow ;echo -n ':'
-    set_color brcyan   ;echo -n (date +%M)
-    set_color brred    ;echo -n ']['
-    set_color brgreen  ;echo -n %self
-    set_color brred    ;echo -n ']['
-    set_color bryellow ;echo -n (df -H (pwd) 2>/dev/null |
+    set_color ffff00 ;echo -n ':'
+    set_color 00ffff ;echo -n (date +%M)
+    set_color ff9900 ;echo -n ']['
+    set_color 00ff00 ;echo -n %self
+    set_color ff9900 ;echo -n ']['
+    set_color ffff00 ;echo -n (df -H (pwd) 2>/dev/null |
                                  sed -n '2p' |
                                  awk '{print $4}' 2>/dev/null)
-    set_color brred    ;echo -n ']'
+    set_color ff9900    ;echo -n ']'
     set fish_prompt_pwd_dir_length 0
-    set_color brwhite  ;echo    ': '(pwd |
+    set_color 000000 ;echo    ': '(pwd |
                                      sed -e "s|\/home\/$USER|\$HOME|g" \
                                          -e 's|\/\/|\/|g' \
                                          -e 's|\/$||g')
   # Line 3
-    set_color bryellow ;echo -n '>'
-    set_color brred    ;echo -n '+'
-    set_color brcyan   ;echo -n 'XX'
-    set_color brgreen  ;echo -n '('
-    set_color brred    ;echo -n '@'    #'^' 'O' '0' '*' 'ϕ' '٭'
-    set_color bryellow ;echo -n '>|'
+    set_color ffff00 ;echo -n '>'
+    set_color ff9900 ;echo -n '+'
+    set_color 00ffff ;echo -n 'XX'
+    set_color 00ff00 ;echo -n '('
+    set_color ff9900 ;echo -n '@'    #'^' 'O' '0' '*' 'ϕ' '٭'
+    set_color ffff00 ;echo -n '>|'
     set_color normal
 end

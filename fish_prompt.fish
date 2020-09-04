00b9bba97dfa22d45955bb2417fa938205496e66
toshi75 <jaf04435@gmail.com>
2020-09-04 15:41:19 +0900

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,15 +1,15 @@
 function fish_prompt --description 'Prompt:noisyfish'
   # toshi75   https://github.com/toshi75/noisyfish
-  
+
   # fisher add jethrokuan/z edc/bass jethrokuan/fzf laughedelic/fish_logo #ryotako/fish-vimcolor
-    
+
   # Line 1
     echo '------------------------------------------'
 
   # Line 2
     set -l USE JP  # JP or not.
     set_color ffff00 ;echo -n 'fish'
-    set_color ff9900 ;echo -n "["
+    set_color ff0000 ;echo -n "["
     set_color 00ffff ;echo -n (date '+%m')
     set_color ffff00 ;echo -n '/'
     set_color 00ffff ;echo -n (date +%d)
@@ -37,13 +37,13 @@ function fish_prompt --description 'Prompt:noisyfish'
                         echo -n (date +%H)
     set_color ffff00 ;echo -n ':'
     set_color 00ffff ;echo -n (date +%M)
-    set_color ff9900 ;echo -n ']['
+    set_color ff0000 ;echo -n ']['
     set_color 00ff00 ;echo -n %self
-    set_color ff9900 ;echo -n ']['
+    set_color ff0000 ;echo -n ']['
     set_color ffff00 ;echo -n (df -H (pwd) 2>/dev/null |
                                  sed -n '2p' |
                                  awk '{print $4}' 2>/dev/null)
-    set_color ff9900    ;echo -n ']'
+    set_color ff0000    ;echo -n ']'
     set fish_prompt_pwd_dir_length 0
     set_color ffff00 ;echo    ': '(pwd |
                                      sed -e "s|\/home\/$USER|\$HOME|g" \
@@ -51,10 +51,10 @@ function fish_prompt --description 'Prompt:noisyfish'
                                          -e 's|\/$||g')
   # Line 3
     set_color ffff00 ;echo -n '>'
-    set_color ff9900 ;echo -n '+'
+    set_color ff0000 ;echo -n '+'
     set_color 00ffff ;echo -n 'XX'
     set_color 00ff00 ;echo -n '('
-    set_color ff9900 ;echo -n '@'    #'^' 'O' '0' '*' 'ϕ' '٭'
+    set_color ff0000 ;echo -n '@'    #'^' 'O' '0' '*' 'ϕ' '٭'
     set_color ffff00 ;echo -n '>|'
     set_color normal
 end

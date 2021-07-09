7d061eeb373eeac946bdc7a0e69532995b969034
toshi75 <jaf04435@gmail.com>
2021-07-09 12:38:26 +0900

    Add files via upload

---
+++ functions/groups.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo groupadd vboxusers
+sudo groupadd vboxsf
+sudo gpasswd -a $USER wheel
+sudo gpasswd -a $USER disk
+sudo gpasswd -a $USER storage
+sudo gpasswd -a $USER video
+sudo gpasswd -a $USER audio
+if (type docker &>/dev/null) ;then
+    sudo gpasswd -a $USER docker
+fi
+
+
+

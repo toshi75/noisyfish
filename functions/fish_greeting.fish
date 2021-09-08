9ce3e2ec80fce66f0a390dc299a27513e1f9c762
toshi75 <jaf04435@gmail.com>
2021-09-08 12:30:07 +0900

    Update fish_greeting.fish

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,4 +1,8 @@
 function fish_greeting
+    if [ -f "$HOME/.config/fish/fisher_update.log" ] ||
+       [ -f "$HOME/.config/fish/completions.log"   ] 
+        auto_update
+    end
     reset
     type fish_logo &>/dev/null
     if [ $status -eq 0 ]

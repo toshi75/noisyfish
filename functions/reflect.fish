b84f7f8307defcea114ded9736f758e5d909e5c3
toshi75 <jaf04435@gmail.com>
2022-02-16 20:34:59 +0900

    Delete reflect.fish

--- functions/reflect.fish
+++
@@ -1,7 +0,0 @@
-function reflect
-    sudo reflector --latest 20 \
-              -p http -p https \
-              --sort rate \
-              --save '/etc/pacman.d/mirrorlist'
-    cat '/etc/pacman.d/mirrorlist'
-end
